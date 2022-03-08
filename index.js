5f1bdf2b45dce68f8a1e2369dae9629155b1f31b
Edwin Fame <edwinfame@yahoo.com>
2022-03-08 12:20:50 -0800

    Commit readme complete outline

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
+
 const promptUser = () => {
   return inquirer.prompt([
     {
       type: 'input',
       name: 'title',
       message: 'What is the title of your project?'
-    }
+    },
     {
       type: 'input',
       name: 'description',
       message: 'How would you describe your project?'
-    }
+    },
     {
       type: 'input',
       name: 'usage',
       message: 'Please provide instructions on how to use your project.'
-    }
+    },
     {
       type: 'input',
       name: 'contribution',
       message: 'What are the guidelines for contributions to this project?'
-    }
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Which license would you like to use for your project?',
+      choices: [Apache, MIT, GNUv3]
+    },
     {
       type: 'input',
       name: 'tests',
-      message: 'Please provide instructions on how to use your project.'
-    }
+      message: 'Please provide instructions tests that could be run.'
+    },
     {
       type: 'input',
       name: 'github',
@@ -37,59 +45,51 @@ const promptUser = () => {
       type: 'input',
       name: 'email',
       message: 'Enter your contact email',
+    },
+  ]);
+};
 
-  }
-  ])
-}
-
-const generateReadme = ({ title, description, installation, usage, contribution, test, location, licence, github, linkedin }) =>
+const generateReadme = ({ title, description, installation, usage, contribution, test, location, licence, github, email }) =>
   `# ${title}
-
+  
   ## Description
   
   ${description}
 
+  ---
   ## Table of Contents 
   
   - [Installation](#installation)
   - [Usage](#usage)
-  - [Credits](#credits)
   - [License](#license)
-  - [Contribution](#how to contribute)
-  - [Contact](#contact)
-  
+  - [Contributions](#contributions)
+  - [Test](#test)
+  - [Questions](#questions)
+
+  ---
   ## Installation
   
- ${installation}
-  
+  ${installation}
+  ---
   ## Usage
-  
   ${usage}
-  
-  ## Credits
-  
-  List your collaborators, if any, with links to their GitHub profiles.
-  
-  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-  
-  If you followed tutorials, include links to those here as well.
-  
+
+  ---
   ## License
   
-  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-  s
+  The last section of a high-quality README file is the license.
+  
   ---
-  
-  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-  
-  ## Badges
-  
-  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-  
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-  
-
   ## How to Contribute
-  
   ${contribution}
-  
+
+  ---
+  ## Test
+  ${test}
+
+  ---
+  ## Questions 
+
+  Please send any questions by email to ${email}.
+  Github https://github.com/${github}.
+`
---
+++ readme.md
@@ -0,0 +1,45 @@
+# ${title}
+
+  ## Description
+  
+  ${description}
+
+---
+  ## Table of Contents 
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Test](#test)
+  - [License](#license)
+  - [Contributions](#contributions)
+  - [Questions](#questions)
+  ---
+  ## Installation
+  
+ ${installation}
+
+  ---
+  ## Usage
+  
+  ${usage}
+
+  ---
+  ## License
+  
+  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  
+  
+  
+  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+---
+  ## Contributions
+  
+  ${contribution}
+  
+---
+  ## Questions 
+
+  Please send any questions by email to ${email}.
+
+  Github https://github.com/${github}.
