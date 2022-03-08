53dafbc5f825d6ca4b26c47b6e0fc2c59864a22c
Edwin Fame <edwinfame@yahoo.com>
2022-03-08 13:09:06 -0800

    added licenses

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const thisyear = Date.prototype.getFullYear()
 
+liscMIT = `Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`
+
+liscGNU = `
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.`
+
+liscApache = ` Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`
 
 const promptUser = () => {
   return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your full name?'
+    }
     {
       type: 'input',
       name: 'title',
@@ -49,7 +97,19 @@ const promptUser = () => {
   ]);
 };
 
-const generateReadme = ({ title, description, installation, usage, contribution, test, location, licence, github, email }) =>
+const lscChoice = ({license}) => {
+  if (license === Apache) {
+    return liscApache
+  }
+
+  else if (license === GNUv3) {
+    return liscGNU
+  }
+
+  else {return liscMIT}
+}
+
+const generateReadme = ({ name, title, description, installation, usage, contribution, test, github, email }) =>
   `# ${title}
   
   ## Description
@@ -77,7 +137,9 @@ const generateReadme = ({ title, description, installation, usage, contribution,
   ---
   ## License
   
-  The last section of a high-quality README file is the license.
+  Copyright (C) ${thisyear}  ${name}
+
+  ${lscChoice}
   
   ---
   ## How to Contribute
@@ -92,4 +154,17 @@ const generateReadme = ({ title, description, installation, usage, contribution,
 
   Please send any questions by email to ${email}.
   Github https://github.com/${github}.
-`
+`;
+
+
+
+const init = () => {
+  promptUser()
+    // Use writeFileSync method to use promises instead of a callback function
+    .then((answers) => lscChoice)
+    .then((answers) => fs.writeFileSync('readmetest.md', generateReadme(answers)))
+    .then(() => console.log('Successfully wrote to readmetest.mdl'))
+    .catch((err) => console.error(err));
+};
+
+init();
